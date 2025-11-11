0b7de1df57b56fdd5e0386be9700a15c592f7bba
akash <aayush45r@gmail.com>
2025-11-11 10:15:19 +0530

    finding the sum of the elements in 2d array!

---
+++ Arrays/maxWealth.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class maxWealth {
+    static  int maximumWealth(int[][] accounts){
+        if(accounts.length == 0){
+            return -1;
+        }
+        int ans = 0;
+        for(int[] account: accounts){
+            int sum = 0;
+            for(int money: account){
+                sum += money;
+            }
+            ans = Math.max(ans,sum);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] account = {
+                {1,3,42,1,2},{12,31,2,31,3}
+        };
+        System.out.println(maximumWealth(account));
+    }
+}
